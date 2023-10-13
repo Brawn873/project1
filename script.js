7444f58f64aba5a2bd60562ecb380a770c1df038
Brawn Idah <idabrawn45@gmail.com>
2023-10-13 20:31:01 +0300

    add event listener for click to pause

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const audio = document.getElementById('audio');
+    const playButton = document.getElementById('play');
+    const pauseButton = document.getElementById('pause');
+    const nextButton = document.getElementById('next');
